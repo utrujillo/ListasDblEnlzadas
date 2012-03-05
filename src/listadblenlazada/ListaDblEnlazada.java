2cc14cd1e5317984697349d70073ace12b64f3a6
Uziel Trujillo Colon <uziel.trujillo@hotmail.com>
2012-03-05 15:58:34 -0600

    Lista Doblemente Enlazada

--- src/listadblenlazada/ListaDblEnlazada.java
+++ src/listadblenlazada/ListaDblEnlazada.java
@@ -1,4 +1,5 @@
 package listadblenlazada;
+import java.util.Scanner;
 /**
  * @author Uziel Trujillo
  */
@@ -6,9 +7,33 @@ public class ListaDblEnlazada {
 
     public static void main(String[] args) {
        dobleEnlaze lde = new dobleEnlaze();
+       Scanner sc = new Scanner(System.in);
        
-       lde.addElemento();
-       lde.printElemento();
-     
+       /*lde.addElemento();
+       lde.printElemento();*/
+       
+       //Ingresamos los valores de la lista
+       lde.addNombre();
+       System.out.println("\nValores de la Lista");
+       lde.printNombre();
+       
+       //Mostramos el Menu y guardamos la opcion seleccionada
+       lde.menu();
+       int opcionMenu = sc.nextInt();
+       
+       //Mientras la opcion no sea 0 => salir
+       while(opcionMenu != 0){
+            
+           //ejecutamos la opcion seleccionada
+            switch(opcionMenu){
+                case 1: { lde.nextNombre(); }; break; //Mover Adelante
+                case 2: { lde.backNombre(); }; break; //Mover Atras
+                case 3: { /*buscar Elemento*/ }; break; //Tarea UAA
+            }
+            
+           //Nuevamente el menu y obtenemos la opcion
+            lde.menu();
+            opcionMenu = sc.nextInt();
+       }//fin del while
     }
 }
