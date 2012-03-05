ca37ad2f3b3678732983029b8c40612397d5cf99
Uziel Trujillo Colon <uziel.trujillo@hotmail.com>
2012-03-05 14:57:21 -0600

    Lista Doblemente Enlazada

---
+++ src/listadblenlazada/ListaDblEnlazada.java
@@ -0,0 +1,14 @@
+package listadblenlazada;
+/**
+ * @author Uziel Trujillo
+ */
+public class ListaDblEnlazada {
+
+    public static void main(String[] args) {
+       dobleEnlaze lde = new dobleEnlaze();
+       
+       lde.addElemento();
+       lde.printElemento();
+     
+    }
+}
