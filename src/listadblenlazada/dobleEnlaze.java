e69e1cc3f60faf1ecdfe8ebdb277d5def740d775
Uziel Trujillo Colon <uziel.trujillo@hotmail.com>
2012-03-05 15:59:53 -0600

    Lista Doblemente Enlazada

--- src/listadblenlazada/dobleEnlaze.java
+++ src/listadblenlazada/dobleEnlaze.java
@@ -65,4 +65,87 @@ public class dobleEnlaze {
             auxiliar = auxiliar.siguiente;
         }
     }
+    
+    
+    
+    public void addNombre(){
+        try{
+        
+            System.out.println("Ingresar Datos, salir -1");
+            int numero = 1;
+            String name = "";
+            
+            while(numero > 0){
+                System.out.print("Numero: ");
+                    numero = sc.nextInt();
+                    
+                if(numero > 0){
+                    System.out.print("Nombre: ");
+                    name = sc.next();
+                    
+                    nodoDblEnlaze nuevoNodo = new nodoDblEnlaze(numero, name);
+                    
+                    if(!estadoLista()){
+                        //nuevoNodo.setSiguiente(this.primerNodo);
+                        nuevoNodo.siguiente = primerNodo;
+                        //this.primerNodo.setAnterior(nuevoNodo);
+                        primerNodo.anterior = nuevoNodo;
+                    }
+
+                    //this.primerNodo = nuevoNodo;
+                    primerNodo = nuevoNodo;
+                    nodoActual = nuevoNodo;
+                    //System.out.println("Elemento Ingresado..");
+                }
+            }
+        }catch(Exception ex){
+            System.out.printf("Ha ocurrido un error: ", ex.getMessage());
+        }
+    }
+    
+    public void printNombre(){
+        nodoDblEnlaze auxiliar = primerNodo;
+        
+        while(auxiliar != null){
+            //System.out.printf("%d ", auxiliar.getDato());
+            System.out.printf("%d %s  |  ", auxiliar.dato, auxiliar.nombre);
+            //auxiliar = auxiliar.getSiguiente();
+            auxiliar = auxiliar.siguiente;
+        }
+    }
+    
+    public void menu(){
+        System.out.print("\n\n");
+        System.out.println("[1] Mover Adelante");
+        System.out.println("[2] Mover Atras");
+        System.out.println("[3] Buscar");
+        System.out.println("[0] Salir");
+    }
+    
+    public void nextNombre(){
+        if(!estadoLista()){
+            if(nodoActual.siguiente != null){
+                nodoActual = nodoActual.siguiente;
+                System.out.printf("\n\nNodo Siguiente: %d -> %s", nodoActual.dato, nodoActual.nombre);
+            }else{
+                System.out.println("El anterior era el ultimo elemento de la lista :P");
+            }
+        }else{
+            System.out.println("La lista esta vacia");
+        }
+    }
+    
+    public void backNombre(){
+        if(!estadoLista()){
+            if(nodoActual.anterior != null){
+                nodoActual = nodoActual.anterior;
+                System.out.printf("\n\nNodo Siguiente: %d -> %s", nodoActual.dato, nodoActual.nombre);
+            }else{
+                System.out.println("El anterior era el ultimo elemento de la lista :P");
+            }
+        }else{
+            System.out.println("La lista esta vacia");
+        }
+    }
+    
 }
